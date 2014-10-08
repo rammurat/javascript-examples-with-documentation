a6466298e4e7abbd6aaacdc785c58b7666214715
romiePhilosophic <rsha68@dluashar985769.sapient.com>
2014-10-08 16:45:03 +0530

    Debugger page introduced

--- app/app.js
+++ app/app.js
@@ -1,26 +1,4 @@
-﻿
-/*#######################################################################
-  
-  Dan Wahlin
-  http://twitter.com/DanWahlin
-  http://weblogs.asp.net/dwahlin
-  http://pluralsight.com/training/Authors/Details/dan-wahlin
-
-  Normally like to break AngularJS apps into the following folder structure
-  at a minimum:
-
-  /app
-      /controllers      
-      /directives
-      /services
-      /partials
-      /views
-
-  #######################################################################*/
-
-
-
-var app = angular.module('OOJSApp', ['ngRoute']);
+﻿var app = angular.module('OOJSApp', ['ngRoute']);
 
 //This configures the routes and associates each route with a view and a controller
 app.config(function ($routeProvider) {
@@ -40,6 +18,16 @@ app.config(function ($routeProvider) {
             {
                 controller: 'OOJSController',
                 templateUrl: '/app/partials/object.html'
+            })
+			.when('/debugger',
+            {
+                controller: 'OOJSController',
+                templateUrl: '/app/partials/browser-debugger.html'
+            })
+			.when('/this',
+            {
+                controller: 'OOJSController',
+                templateUrl: '/app/partials/this.html'
             })
         .otherwise({ redirectTo: '/js' });
 });
